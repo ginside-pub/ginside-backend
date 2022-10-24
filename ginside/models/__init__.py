d8eb1456806552e6638f08cf06d72d7adc4971c8
0xDEAFBABE <deafbabe@startmail.com>
2022-10-24 17:50:14 +0600

    Add sample model

--- ginside/models/__init__.py
+++ ginside/models/__init__.py
@@ -1,1 +1,7 @@
-"""There will be imports of all models."""
+from .sample import SampleDoesNotExistError  # noqa: F401
+from .sample import SampleNameOccupiedError  # noqa: F401
+from .sample import sample_create  # noqa: F401
+from .sample import sample_delete  # noqa: F401
+from .sample import sample_get  # noqa: F401
+from .sample import sample_get_list  # noqa: F401
+from .sample import sample_update  # noqa: F401
---
+++ ginside/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .sample import SampleCreate  # noqa: F401
+from .sample import SampleGet  # noqa: F401
+from .sample import SampleGetList  # noqa: F401
+from .sample import SampleUpdate  # noqa: F401
