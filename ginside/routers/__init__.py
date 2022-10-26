c893a1d7112a5a6e9cafff2af1688964aa51d611
Euromantic <deafbabe@startmail.com>
2022-10-26 16:41:42 +0600

    Remove samples (#6)

--- ginside/routers/__init__.py
+++ ginside/routers/__init__.py
@@ -2,13 +2,11 @@ from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 
 from .posts import router as posts_router
-from .samples import router as samples_router
 
 
 router = APIRouter()
 
 router.include_router(posts_router, prefix='/posts', tags=['posts'])
-router.include_router(samples_router, prefix='/samples', tags=['samples'])
 
 
 @router.get('/', include_in_schema=False)
